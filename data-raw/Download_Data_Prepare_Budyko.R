b41c3cb95c2cffd0469f55795e514029bc719ed4
Anna Grossmann <anna.grossmann@students.unibe.ch>
2025-05-21 14:49:16 +0200

    downloads

---
+++ data-raw/Download_Data_Prepare_Budyko.R
@@ -0,0 +1,40 @@
+
+install.packages("dplyr")
+library(dplyr)
+install.packages("terra")
+library(terra)
+install.packages("remotes")
+library(remotes)
+remotes::install_github("https://github.com/geco-bern/FluxDataKit")
+library(FluxDataKit)
+
+library(tidyverse)
+library(readr)
+
+
+
+
+## Get site infos and fullyearsequences from FluxDataKit v.3.4
+
+df_sites <- readr::read_csv("/data_2/FluxDataKit/v3.4/zenodo_upload/fdk_site_info.csv") |>
+  filter(!(sitename %in% c("MX-Tes", "US-KS3"))) |>  # failed sites
+  left_join(
+    readr::read_csv("/data_2/FluxDataKit/v3.4/zenodo_upload/fdk_site_fullyearsequence.csv"),
+    by = "sitename"
+  )
+
+
+##ADD CTI Data
+cti_all_data <- rast("/data/archive/gti_marthews_2015/data/ga2.nc")
+
+# extract needed Value from cti_all_data
+extracted_cti <- terra::extract(
+  cti_all_data,
+  df_sites |> dplyr::select(lon, lat)
+)
+
+df_sites <- df_sites |>
+  dplyr::mutate(cti = extracted_cti[[2]])
+
+
+
